fae43d1b79734f37004b425f2bc35ec291b4e09f
Annenkov Maksim <bakeeva@yandex.ru>
2024-06-18 15:19:56 +0300

    save one

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        //TODO Количество несовершеннолетних (возраст менее 18 лет)
+        long underages = persons.stream().filter(person -> person.getAge() < 18).count();
+
+        // TODO Список фамилий мужчин призывного возраста (возраст от 18  до 27 лет)
+        List<String> recruit = persons.stream().filter(person -> (person.getAge() >= 18 && person.getAge() <= 27))
+                .filter(person -> person.getSex() == Sex.MAN).map(Person::getFamily)
+                .collect(Collectors.toList());
+
+        //TODO Отсортированный по фамилии список работоспособных людей
+        // с высшим образованием c дополнительными условиями (то есть людей с высшим образованием
+        // от 18 до 60 лет для женщин,
+        // и до 65 лет для мужчин)
+        Collection<Person> ableBodied = persons.stream().
+                filter(person -> person.getEducation() == Education.HIGHER)
+                .filter(person -> person.getSex() == Sex.MAN ? person.getAge() >= 18 && person.getAge() <= 65
+                        : person.getAge() >= 18 && person.getAge() <= 60)
+                .sorted(Comparator.comparing(Person::getFamily)).collect(Collectors.toList());
+    }
+}
